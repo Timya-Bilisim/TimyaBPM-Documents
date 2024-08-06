8cb94fa8a81d7d99d8454bf7921f1d071bdac64c
NURŞAH YILDIZ <nursah.yildiz@timyabilisim.com.tr>
2024-08-06 12:06:56 +0300

    başlangıç giriş eklendi
    başlangıç giriş eklendi

--- docs/.vuepress/.temp/pages/getting-started.html.vue
+++ docs/.vuepress/.temp/pages/getting-started.html.vue
@@ -1,14 +1,20 @@
 <template><div><h1 id="baslangıc" tabindex="-1"><a class="header-anchor" href="#baslangıc"><span>Başlangıç</span></a></h1>
-<h2 id="_1-is-baslat" tabindex="-1"><a class="header-anchor" href="#_1-is-baslat"><span>1. İş Başlat</span></a></h2>
+<h2 id="_1-timya-bpm-sisteme-giris-yapma" tabindex="-1"><a class="header-anchor" href="#_1-timya-bpm-sisteme-giris-yapma"><span>1. Timya/BPM Sisteme Giriş Yapma</span></a></h2>
+<p>Kullanıcı adı , şifre ve giriş tipi seçilerek  sisteme giriş yapılır. Giriş tipleri ikiye ayrılır: Basit ve Active Directory. Admin kullanıcı adıyla giriş yapılacaksa, giriş tipi basit olarak seçilmelidir. Şirket tarafından belirlenen özel kullanıcı adı ve şifre ile giriş yapılacaksa, Active Directory seçilmelidir.</p>
+<div style="display: flex; justify-content: space-between;">
+    <img src="/TimyaBPM-Documents/adminn.png" alt="Admin 1" style="width: 48%;">
+    <img src="/TimyaBPM-Documents/admin2.png" alt="Admin 2" style="width: 48%;">
+</div>
+<h2 id="_2-is-baslat" tabindex="-1"><a class="header-anchor" href="#_2-is-baslat"><span>2. İş Başlat</span></a></h2>
 <p>Kullanıma açılmış olan süreçlerin hangi aksiyon ile başlatılabileceğini görebilirseniz. Bir aksiyonu temsil eden butona tıklayarak süreci başlatabilirsiniz. Süreçler genel olarak bir form ile başlar. Form açıldıktan sonra bir sonraki aşamaya iletilmeyen işler yapılacak işler sekmesinde kalmaya devam eder.</p>
-<h2 id="_2-yapılacak-isler" tabindex="-1"><a class="header-anchor" href="#_2-yapılacak-isler"><span>2. Yapılacak İşler</span></a></h2>
+<h2 id="_3-yapılacak-isler" tabindex="-1"><a class="header-anchor" href="#_3-yapılacak-isler"><span>3. Yapılacak İşler</span></a></h2>
 <p>Başlattığınız ve tamamlamadığınız işler ya da size yapmanız için iletilmiş olan işler bu ekranda listelenir. Listelenmiş işlere satırlara tıklayarak açabilir ve işin içeriğini görüntüleyebilirsiniz.</p>
-<h2 id="_3-takip-edilen-isler" tabindex="-1"><a class="header-anchor" href="#_3-takip-edilen-isler"><span>3. Takip Edilen İşler</span></a></h2>
+<h2 id="_4-takip-edilen-isler" tabindex="-1"><a class="header-anchor" href="#_4-takip-edilen-isler"><span>4. Takip Edilen İşler</span></a></h2>
 <p>Takip etmeniz için görüntüleyebileceğiniz işler bu ekranda listelenir.  Burada görüntülenen işlerin neler olacağı süreçlerin tasarımı aşamasında belirlenir.</p>
 <p><img src="/TimyaBPM-Documents/images/image-3.png" alt="alt text"></p>
-<h2 id="_4-raporlar" tabindex="-1"><a class="header-anchor" href="#_4-raporlar"><span>4. Raporlar</span></a></h2>
+<h2 id="_5-raporlar" tabindex="-1"><a class="header-anchor" href="#_5-raporlar"><span>5. Raporlar</span></a></h2>
 <p>Devam eden ya da tamamlanmış olan işlerin önceden belirlenmiş formatlara göre raporlanabileceği ekrandır.</p>
-<h2 id="_5-formlar" tabindex="-1"><a class="header-anchor" href="#_5-formlar"><span>5. Formlar</span></a></h2>
+<h2 id="_6-formlar" tabindex="-1"><a class="header-anchor" href="#_6-formlar"><span>6. Formlar</span></a></h2>
 <p>Bir sürecin parçası olmayan formlar bu sekmede görüntülenir. Genel olarak ayarlar için kullanılır.</p>
 </div></template>
 
--- docs/getting-started.md
+++ docs/getting-started.md
@@ -1,28 +1,35 @@
-
 # Başlangıç
 
+## 1. Timya/BPM Sisteme Giriş Yapma
 
-## 1. İş Başlat
+Kullanıcı adı , şifre ve giriş tipi seçilerek  sisteme giriş yapılır. Giriş tipleri ikiye ayrılır: Basit ve Active Directory. Admin kullanıcı adıyla giriş yapılacaksa, giriş tipi basit olarak seçilmelidir. Şirket tarafından belirlenen özel kullanıcı adı ve şifre ile giriş yapılacaksa, Active Directory seçilmelidir.
+
+<div style="display: flex; justify-content: space-between;">
+    <img src="/TimyaBPM-Documents/adminn.png" alt="Admin 1" style="width: 48%;">
+    <img src="/TimyaBPM-Documents/admin2.png" alt="Admin 2" style="width: 48%;">
+</div>
+
+## 2. İş Başlat
 Kullanıma açılmış olan süreçlerin hangi aksiyon ile başlatılabileceğini görebilirseniz. Bir aksiyonu temsil eden butona tıklayarak süreci başlatabilirsiniz. Süreçler genel olarak bir form ile başlar. Form açıldıktan sonra bir sonraki aşamaya iletilmeyen işler yapılacak işler sekmesinde kalmaya devam eder.
 
 
 
 
 
-## 2. Yapılacak İşler
+## 3. Yapılacak İşler
 Başlattığınız ve tamamlamadığınız işler ya da size yapmanız için iletilmiş olan işler bu ekranda listelenir. Listelenmiş işlere satırlara tıklayarak açabilir ve işin içeriğini görüntüleyebilirsiniz.
 
 
 
-## 3. Takip Edilen İşler
+## 4. Takip Edilen İşler
 Takip etmeniz için görüntüleyebileceğiniz işler bu ekranda listelenir.  Burada görüntülenen işlerin neler olacağı süreçlerin tasarımı aşamasında belirlenir.
 
 ![alt text](/TimyaBPM-Documents/images/image-3.png)
 
-## 4. Raporlar
+## 5. Raporlar
 Devam eden ya da tamamlanmış olan işlerin önceden belirlenmiş formatlara göre raporlanabileceği ekrandır.
 
-## 5. Formlar
+## 6. Formlar
 Bir sürecin parçası olmayan formlar bu sekmede görüntülenir. Genel olarak ayarlar için kullanılır.
 
  
