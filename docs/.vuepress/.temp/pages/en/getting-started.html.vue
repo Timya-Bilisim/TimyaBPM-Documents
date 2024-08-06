d4f7dfd165a7fac9c50ebd571567ff0fd8873f96
NURŞAH YILDIZ <nursah.yildiz@timyabilisim.com.tr>
2024-08-06 12:12:32 +0300

    ingilizce başlangıç değiştirildi.
    ingilizce başlangıç değiştirildi.

--- docs/.vuepress/.temp/pages/en/getting-started.html.vue
+++ docs/.vuepress/.temp/pages/en/getting-started.html.vue
@@ -1,14 +1,20 @@
 <template><div><h1 id="start-up" tabindex="-1"><a class="header-anchor" href="#start-up"><span>Start Up</span></a></h1>
-<h2 id="_1-start-process" tabindex="-1"><a class="header-anchor" href="#_1-start-process"><span>1. Start Process</span></a></h2>
+<h2 id="_1-timya-bpm-system-login" tabindex="-1"><a class="header-anchor" href="#_1-timya-bpm-system-login"><span>1. Timya/BPM System Login</span></a></h2>
+<p>Login to the system using a username, password, and selected login type. There are two login types: Basit and Active Directory. If logging in with an admin username, the login type should be set to Basit. If logging in with a company-specified username and password, the login type should be set to Active Directory.</p>
+<div style="display: flex; justify-content: space-between;">
+    <img src="/TimyaBPM-Documents/adminn.png" alt="Admin 1" style="width: 48%;">
+    <img src="/TimyaBPM-Documents/admin2.png" alt="Admin 2" style="width: 48%;">
+</div>
+<h2 id="_2-start-process" tabindex="-1"><a class="header-anchor" href="#_2-start-process"><span>2. Start Process</span></a></h2>
 <p>Can see how processes that are ready for use can be initiated with an action. Click on the button representing an action to start the process. Processes typically start with a form. If not forwarded to the next stage after the form is opened, tasks will continue to stay in the &quot;to-do tasks&quot; tab.</p>
-<h2 id="_2-to-do-tasks" tabindex="-1"><a class="header-anchor" href="#_2-to-do-tasks"><span>2. To-Do Tasks</span></a></h2>
+<h2 id="_3-to-do-tasks" tabindex="-1"><a class="header-anchor" href="#_3-to-do-tasks"><span>3. To-Do Tasks</span></a></h2>
 <p>Tasks that you have started and not completed, or tasks assigned to you to complete, are listed on this screen. Click on the rows of listed tasks to open and view the details of the task.</p>
-<h2 id="_3-followed-tasks" tabindex="-1"><a class="header-anchor" href="#_3-followed-tasks"><span>3. Followed Tasks</span></a></h2>
+<h2 id="_4-followed-tasks" tabindex="-1"><a class="header-anchor" href="#_4-followed-tasks"><span>4. Followed Tasks</span></a></h2>
 <p>Tasks that you are following and can view are listed on this screen. The specifics of these tasks are determined during the process design phase.</p>
 <p><img src="/TimyaBPM-Documents/images/image-3.png" alt="alt text"></p>
-<h2 id="_4-reports" tabindex="-1"><a class="header-anchor" href="#_4-reports"><span>4. Reports</span></a></h2>
+<h2 id="_5-reports" tabindex="-1"><a class="header-anchor" href="#_5-reports"><span>5. Reports</span></a></h2>
 <p>This screen allows you to generate reports for ongoing or completed tasks in predefined formats.</p>
-<h2 id="_5-forms" tabindex="-1"><a class="header-anchor" href="#_5-forms"><span>5. Forms</span></a></h2>
+<h2 id="_6-forms" tabindex="-1"><a class="header-anchor" href="#_6-forms"><span>6. Forms</span></a></h2>
 <p>Forms that are not part of a process are displayed on this tab. It is generally used for settings.</p>
 </div></template>
 
--- docs/en/getting-started.md
+++ docs/en/getting-started.md
@@ -1,20 +1,32 @@
 # Start Up
 
-## 1. Start Process
+
+## 1. Timya/BPM System Login
+Login to the system using a username, password, and selected login type. There are two login types: Basit and Active Directory. If logging in with an admin username, the login type should be set to Basit. If logging in with a company-specified username and password, the login type should be set to Active Directory.
+
+
+
+<div style="display: flex; justify-content: space-between;">
+    <img src="/TimyaBPM-Documents/adminn.png" alt="Admin 1" style="width: 48%;">
+    <img src="/TimyaBPM-Documents/admin2.png" alt="Admin 2" style="width: 48%;">
+</div>
+
+
+## 2. Start Process
 Can see how processes that are ready for use can be initiated with an action. Click on the button representing an action to start the process. Processes typically start with a form. If not forwarded to the next stage after the form is opened, tasks will continue to stay in the "to-do tasks" tab.
 
 
 
-## 2. To-Do Tasks
+## 3. To-Do Tasks
 Tasks that you have started and not completed, or tasks assigned to you to complete, are listed on this screen. Click on the rows of listed tasks to open and view the details of the task.
 
-## 3. Followed Tasks
+## 4. Followed Tasks
 Tasks that you are following and can view are listed on this screen. The specifics of these tasks are determined during the process design phase.
 
 ![alt text](/TimyaBPM-Documents/images/image-3.png)
 
-## 4. Reports
+## 5. Reports
 This screen allows you to generate reports for ongoing or completed tasks in predefined formats.
 
-## 5. Forms
+## 6. Forms
 Forms that are not part of a process are displayed on this tab. It is generally used for settings.
