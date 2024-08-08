5381d776ac14c7e1344e7e453f9cda69a01cc0dc
NURŞAH YILDIZ <nursah.yildiz@timyabilisim.com.tr>
2024-08-08 14:37:57 +0300

    veri tanimlari

--- docs/.vuepress/.temp/pages/en/veritanim.html.vue
+++ docs/.vuepress/.temp/pages/en/veritanim.html.vue
@@ -1,19 +1,19 @@
 <template><div><h1 id="data-specifications" tabindex="-1"><a class="header-anchor" href="#data-specifications"><span>Data Specifications</span></a></h1>
 <h2 id="_1-data-definitions" tabindex="-1"><a class="header-anchor" href="#_1-data-definitions"><span>1. Data Definitions</span></a></h2>
 <p>Data that is not in a table format and does not consist of multiple entries is stored in data definitions. Data is entered by clicking &quot;Add&quot;. Press Enter to input the data. After all data has been entered, click the save button.</p>
-<p>![Data Definition Screenshot 1]( /TimyaBPM-Documents/vt1.png &quot;alt text&quot; width=&quot;800&quot;/)</p>
+<img src="/TimyaBPM-Documents/vt1.png" alt="alt text" width="800"/>
 <h3 id="_1-1-key" tabindex="-1"><a class="header-anchor" href="#_1-1-key"><span>1.1. Key</span></a></h3>
 <p>Users whose data will be stored, that is, data rows, are created. For example, in the form, a section for Title Subject can be named as shown in the picture:</p>
-<p>![Data Definition Screenshot 2]( /TimyaBPM-Documents/vt2.png &quot;alt text&quot; width=&quot;400&quot;/)</p>
+<img src="/TimyaBPM-Documents/vt2.png" alt="alt text" width="400"/>
 <h3 id="_1-2-tablename" tabindex="-1"><a class="header-anchor" href="#_1-2-tablename"><span>1.2. Tablename</span></a></h3>
 <p>The name of the table where data is stored in the database is entered.</p>
-<p>![Data Definition Screenshot 3]( /TimyaBPM-Documents/vt3.png &quot;alt text&quot; width=&quot;400&quot;/)</p>
+<img src="/TimyaBPM-Documents/vt3.png" alt="alt text" width="400"/>
 <h3 id="_1-3-name" tabindex="-1"><a class="header-anchor" href="#_1-3-name"><span>1.3. Name</span></a></h3>
 <p>Must be the same as the Key section.</p>
-<p>![Data Definition Screenshot 4]( /TimyaBPM-Documents/vt4.png &quot;alt text&quot; width=&quot;400&quot;/)</p>
+<img src="/TimyaBPM-Documents/vt4.png" alt="alt text" width="400"/>
 <h3 id="_1-4-description" tabindex="-1"><a class="header-anchor" href="#_1-4-description"><span>1.4. Description</span></a></h3>
 <p>Not mandatory, can only be seen by the creator.</p>
-<p>![Data Definition Screenshot 5]( /TimyaBPM-Documents/vt5.png &quot;alt text&quot; width=&quot;400&quot;/)</p>
+<img src="/TimyaBPM-Documents/vt5.png" alt="alt text" width="400"/>
 <h3 id="_1-5-datavaluetype" tabindex="-1"><a class="header-anchor" href="#_1-5-datavaluetype"><span>1.5. DataValueType</span></a></h3>
 <p>Select the data type. It can be one of the following options:</p>
 <ul>
@@ -23,10 +23,10 @@
 <li><strong>TIME:</strong> Used to store only time information.</li>
 <li><strong>MONEY:</strong> Used for currency data.</li>
 </ul>
-<p>![Data Definition Screenshot 6]( /TimyaBPM-Documents/vt6.png &quot;alt text&quot; width=&quot;500&quot;/)</p>
+<img src="/TimyaBPM-Documents/vt6.png" alt="alt text" width="500"/>
 <h3 id="_1-6-length" tabindex="-1"><a class="header-anchor" href="#_1-6-length"><span>1.6. Length</span></a></h3>
 <p>The length of the data is determined.</p>
-<p>![Data Definition Screenshot 7]( /TimyaBPM-Documents/vt7.png &quot;alt text&quot; width=&quot;600&quot;/)</p>
+<img src="/TimyaBPM-Documents/vt7.png" alt="alt text" width="600"/>
 </div></template>
 
 
--- docs/en/veritanim.md
+++ docs/en/veritanim.md
@@ -4,31 +4,31 @@
 
 Data that is not in a table format and does not consist of multiple entries is stored in data definitions. Data is entered by clicking "Add". Press Enter to input the data. After all data has been entered, click the save button.
 
-![Data Definition Screenshot 1]( /TimyaBPM-Documents/vt1.png "alt text" width="800"/)
+<img src="/TimyaBPM-Documents/vt1.png" alt="alt text" width="800"/>
 
 ### 1.1. Key
 
 Users whose data will be stored, that is, data rows, are created. For example, in the form, a section for Title Subject can be named as shown in the picture:
 
-![Data Definition Screenshot 2]( /TimyaBPM-Documents/vt2.png "alt text" width="400"/)
+<img src="/TimyaBPM-Documents/vt2.png" alt="alt text" width="400"/>
 
 ### 1.2. Tablename
 
 The name of the table where data is stored in the database is entered.
 
-![Data Definition Screenshot 3]( /TimyaBPM-Documents/vt3.png "alt text" width="400"/)
+<img src="/TimyaBPM-Documents/vt3.png" alt="alt text" width="400"/>
 
 ### 1.3. Name
 
 Must be the same as the Key section.
 
-![Data Definition Screenshot 4]( /TimyaBPM-Documents/vt4.png "alt text" width="400"/)
+<img src="/TimyaBPM-Documents/vt4.png" alt="alt text" width="400"/>
 
 ### 1.4. Description
 
 Not mandatory, can only be seen by the creator.
 
-![Data Definition Screenshot 5]( /TimyaBPM-Documents/vt5.png "alt text" width="400"/)
+<img src="/TimyaBPM-Documents/vt5.png" alt="alt text" width="400"/>
 
 ### 1.5. DataValueType
 
@@ -40,11 +40,10 @@ Select the data type. It can be one of the following options:
 - **TIME:** Used to store only time information.
 - **MONEY:** Used for currency data. 
 
-![Data Definition Screenshot 6]( /TimyaBPM-Documents/vt6.png "alt text" width="500"/)
+<img src="/TimyaBPM-Documents/vt6.png" alt="alt text" width="500"/>
 
 ### 1.6. Length
 
 The length of the data is determined.
 
-![Data Definition Screenshot 7]( /TimyaBPM-Documents/vt7.png "alt text" width="600"/)
-
+<img src="/TimyaBPM-Documents/vt7.png" alt="alt text" width="600"/>
